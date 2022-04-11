baf2be6b346e81af0e5ee3f023ae01ec809942aa
Own Hee Yun <56327729+wonhee3472@users.noreply.github.com>
2022-04-11 09:31:50 -0500

    a form to submit data in a local SQL server
    Created a web form that submits data through it using ASP.NET, C#, and MS SQL Server.

---
+++ Default.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+
+public partial class _Default : System.Web.UI.Page
+{
+    SqlConnection c = new SqlConnection("Data Source=OWNHEEY2WINLAP\\MSSQLSERVER2; Initial Catalog=usersDB; User Id=sa; Password=dPtnsla4fka!");
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void btnSubmit_Click(object sender, EventArgs e)
+    {
+        c.Open();
+        SqlCommand cmd = new SqlCommand("insert into userInfo values('" + txtWolframID.Text + "','" + txtPassword.Text + "','" + txtName.Text + "','" + txtAddress.Text + "','" + txtMobile.Text + "','" + txtEmail.Text + "')", c);
+        cmd.ExecuteNonQuery();
+        lblMessage.Text = "Record submitted successfully";
+        c.Close();
+    }
+}
