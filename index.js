17771fe4ff7f8e2ac43370da7544fe0f62ce8d52
Own Hee Yun <ownheey@wolfram.com>
2022-04-12 14:41:54 -0500

    adding the cod for the last prompt

---
+++ index.js
@@ -0,0 +1,37 @@
+function init() {
+    data = [{
+        x: [1, 2, 3, 4, 5],
+        y: [1, 2, 4, 8, 16],
+        type: 'bar' 
+      }];
+      Plotly.newPlot('plot', data);
+  };
+  
+  d3.selectAll("#dropdownMenu").on("change", updatePlotly);
+
+  function updatePlotly() {
+    var dropdownMenu = d3.select("#dropdownMenu");
+    var dataset = dropdownMenu.property("value");
+  
+    var xData = [];
+    var yData = [];
+  
+    if (dataset === 'Question1') {
+      xData = ["Male", "Female"],
+      yData = [4, 1];
+    };
+  
+    if (dataset === 'Question2') {
+      xData = ["SF", "Champaign", "LA"]
+      yData = [2, 2, 1];
+    };
+  
+    var trace = {
+      x: [xData],
+      y: [yData],
+    };
+    Plotly.restyle("plot", trace);
+  };
+  
+ 
+  init();
